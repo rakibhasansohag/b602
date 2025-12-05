40d7c8e2b5cb68f9d0c744a8143457884c1ab0de
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-12-05 23:02:02 +0600

    adding the logger for our project

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import initDB from './config/db';
 import errorHandler from './middleware/errorHandler';
+import logger from './middleware/logger';
 
 const app = express();
 
@@ -10,13 +11,15 @@ initDB().catch((err) => {
 	process.exit(1);
 });
 
-app.use('/', (req, res) => {
+app.use('/', (_req, res) => {
 	res.send('Car is running WIthOut Typescript!');
 });
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(logger);
+
 
 // not found
 app.use((_req, res) => {

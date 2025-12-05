7d39f76b785bd92d4daf1849c2359c043055dd47
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-12-05 22:14:10 +0600

    initialize the project

---
+++ open.bat
@@ -0,0 +1,5 @@
+@echo off
+
+start wt ^
+  new-tab -p "PowerShell" --title "Assignment 2 Dev" -d "F:\programming-hero-level-2\milestone-3\b602-express-vehechile" powershell -NoExit -Command "npm run dev" ; ^
+  new-tab -p "PowerShell" --title "General" -d "F:\programming-hero-level-2\milestone-3\b602-express-vehechile" powershell -NoExit

348485916cc006dc94b33db032b3e4d3d61fd5bb
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-12-07 23:34:18 +0600

    zod related all code removed

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -1,28 +1,37 @@
 import { Router } from 'express';
 import { authController } from './auth.controller';
-import validate from '../../middleware/validate';
-import { z } from 'zod';
+// import validate from '../../middleware/validate';
+// import { z } from 'zod';
 
 const router = Router();
 
-const signupSchema = z.object({
-	body: z.object({
-		name: z.string().min(1),
-		email: z.email(),
-		password: z.string().min(6),
-		phone: z.string().min(6),
-		role: z.enum(['admin', 'customer']).optional(),
-	}),
-});
+// const signupSchema = z.object({
+// 	body: z.object({
+// 		name: z.string().min(1),
+// 		email: z.email(),
+// 		password: z.string().min(6),
+// 		phone: z.string().min(6),
+// 		role: z.enum(['admin', 'customer']).optional(),
+// 	}),
+// });
 
-const signinSchema = z.object({
-	body: z.object({
-		email: z.email(),
-		password: z.string().min(6),
-	}),
-});
+// const signinSchema = z.object({
+// 	body: z.object({
+// 		email: z.email(),
+// 		password: z.string().min(6),
+// 	}),
+// });
 
-router.post('/signup', validate(signupSchema), authController.signup);
-router.post('/signin', validate(signinSchema), authController.signin);
+router.post(
+	'/signup',
+	// validate(signupSchema),
+	authController.signup,
+);
+
+router.post(
+	'/signin',
+	//  validate(signinSchema),
+	authController.signin,
+);
 
 export default router;
--- src/modules/users/user.route.ts
+++ src/modules/users/user.route.ts
@@ -1,8 +1,8 @@
 import { Router } from 'express';
 import { userController } from './user.controller';
 import auth from '../../middleware/auth';
-import validate from '../../middleware/validate';
-import { z } from 'zod';
+// import validate from '../../middleware/validate';
+// import { z } from 'zod';
 
 const router = Router();
 
@@ -12,19 +12,19 @@ router.get('/', auth('admin'), userController.getAllUsers);
 // router.get('/:id', auth('admin', 'customer'), userController.getUser);
 
 // admin or owner update: we'll validate body with zod
-const updateSchema = z.object({
-	body: z.object({
-		name: z.string().optional(),
-		email: z.email(),
-		phone: z.string().optional(),
-		role: z.enum(['admin', 'customer']).optional(),
-	}),
-});
+// const updateSchema = z.object({
+// 	body: z.object({
+// 		name: z.string().optional(),
+// 		email: z.email(),
+// 		phone: z.string().optional(),
+// 		role: z.enum(['admin', 'customer']).optional(),
+// 	}),
+// });
 
 router.put(
 	'/:id',
 	auth('admin', 'customer'),
-	validate(updateSchema),
+	// validate(updateSchema),
 	userController.updateUser,
 );
 router.delete('/:id', auth('admin'), userController.deleteUser);
